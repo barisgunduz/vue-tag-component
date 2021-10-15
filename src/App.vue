66903fd63cbfda3f89d563625ebc528ee1da5cb2
Barış Gündüz <baris.gunduz@devstup.org>
2021-10-15 18:23:53 +0300

    project done

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,31 @@
 <template>
-    <div class="tag-container">
-        <span class="tag">
-            <span class="content">Vuejs</span>
-            <span class="close">x</span>
-        </span>
+    <div class="container">
+      <app-tags v-model="tags" color="primary"></app-tags>
+      {{tags}}
+      <hr>
+      <app-tags v-model="tags2" ></app-tags>
+      {{tags2}}
+      <hr>
     </div>
 </template>
 
 <script>
-export default {};
+import Tags from "./components/Tags"
+export default {
+    components : {
+      appTags : Tags
+    },
+    data(){
+      return {
+        tags: "deneme, test, barış",
+        tags2: "deneme2, test2, barış2"
+      }
+    }
+};
 </script>
 
 <style>
 body {
     font-family: sans-serif;
-}
-.tag-container {
-    border: 1px solid #ccc;
-    padding: 20px;
-}
-.tag {
-    background-color: #fbdd08;
-    padding: 10px;
-    color: #000;
-    font-size: 14px;
-}
-
-.tag .close {
-  font-size: 12px;
-  cursor: pointer;
 }
 </style>
---
+++ src/components/Tag.vue
@@ -0,0 +1,38 @@
+<template>
+    <span class="tag" :class="selectedTheme">
+        <span class="content">{{ tag }}</span>
+        <span class="close" @click="$emit('removeOneTagEvent', index)">x</span>
+    </span>
+</template>
+
+<script>
+export default {
+    props: ["tag", "index", "tagColor"],
+    data() {
+        return {
+            selectedTheme: null,
+        };
+    },
+    created() {
+        this.selectedTheme = this.tagColor;
+    },
+};
+</script>
+
+<style scoped>
+.primary {
+    color: #004085;
+    background-color: #cce5ff;
+    border-color: #b8daff;
+}
+.tag {
+    padding: 10px;
+    font-size: 14px;
+    margin-right: 10px;
+}
+
+.tag .close {
+    font-size: 12px;
+    cursor: pointer;
+}
+</style>
